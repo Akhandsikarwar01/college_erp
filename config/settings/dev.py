ecb09a67cc32214af88d3dabb8a276f2c23c2ad3
Akhand Sikarwar <144526034+Akhandsikarwar01@users.noreply.github.com>
2026-03-03 22:15:20 +0530

    Clean settings file properly

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,23 +1,17 @@
 from .base import *
 
 DEBUG = False
-HEAD
-ALLOWED_HOSTS = [
-    "college-erp-vlo5.onrender.com",
-    "akhandsikarwar.in",
-    "www.akhandsikarwar.in",
-]
-22cc4cb (Production settings fix)
 
 ALLOWED_HOSTS = [
     "college-erp-vlo5.onrender.com",
     "akhandsikarwar.in",
     "www.akhandsikarwar.in",
 ]
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "erpmanager.akhand@gmail.com"
-EMAIL_HOST_PASSWORD = "wkfc pfkf fygb wldu"  # spaces hata ke
+EMAIL_HOST_PASSWORD = "wkfc pfkf fygb wldu"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
