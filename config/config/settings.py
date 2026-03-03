9434d76ba97531140317324d63c54c78f7ce9c9c
Akhand Sikarwar <144526034+Akhandsikarwar01@users.noreply.github.com>
2026-03-03 23:00:08 +0530

    Temp allow all hosts

--- config/config/settings.py
+++ config/config/settings.py
@@ -7,13 +7,7 @@ SECRET_KEY = 'django-insecure-1p5ilzx66^#+a97_o6wnx2*7uj9&zwj%^7xsg!i8hf%arzjttz
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    ".onrender.com",
-    "erp.akhandsikarwar.in",
-    "college-erp-vlo5.onrender.com",
-    "akhandsikarwar.in",
-    "www.akhandsikarwar.in",
-]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
